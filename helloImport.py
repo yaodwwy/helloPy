ebd67580eb05d7ded704ebb9f4eb1941979a6575
Adam.Yao <yaoo_yao@gomro.cn>
2017-12-27 14:30:26 +0800

    start

--- helloImport.py
+++ helloImport.py
@@ -2,23 +2,22 @@
 # Windows 下可以不写第一行注释:
 
 import sys
+from sys import path  # 导入特定的成员
 
-print('================Python import mode==========================');
+print('================Python import mode==========================')
 print('命令行参数为:')
 for i in sys.argv:
     print(i)
 print('\n python 路径为', sys.path)
-
-from sys import argv, path  # 导入特定的成员
 
 print('================python from import===================================')
 print('path:', path)  # 因为已经导入path成员，所以此处引用时不需要加sys.path
 
 
 def example(anything):
-    '''形参为任意类型的对象，
+    """形参为任意类型的对象，
        这个示例函数会将其原样返回。
-    '''
+    """
     return anything + 'ok'
 
 
